ecdcbc261e89319d75ecef9a75f6c626c5f08e7d
Hengyu <h3ngyu@gmail.com>
2023-03-30 10:22:52 +0800

    Add: now settings will show on hover

--- src/components/PluginList.js
+++ src/components/PluginList.js
@@ -1,10 +1,21 @@
 import React from "react";
 import PluginItem from "./PluginItem";
 import { compactLesson } from "../plugins/CompactLesson";
+import { useState } from "react";
 
 const PluginList = ({ origin }) => {
+    const [isHover, setHover] = useState(false);
+
     return (
-        <div className="dropdown mx-3">
+        <div
+            className="dropdown mx-3"
+            onMouseEnter={() => {
+                setHover(true);
+            }}
+            onMouseLeave={() => {
+                setHover(false);
+            }}
+        >
             <button
                 className="btn dropdown-toggle btn-link text-light"
                 data-bs-toggle="dropdown"
@@ -14,7 +25,16 @@ const PluginList = ({ origin }) => {
                 <i className="fa fa-cog fa-lg" aria-hidden="true"></i>
             </button>
 
-            <ul class="dropdown-menu">
+            <ul
+                class="dropdown-menu"
+                style={
+                    isHover
+                        ? {
+                              display: "block",
+                          }
+                        : {}
+                }
+            >
                 <li>
                     <PluginItem
                         text={"紧密的课程布局"}
