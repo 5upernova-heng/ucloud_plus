e3438eb9d4f35ba743dcb94106f1b30796f1dbf3
Hengyu <h3ngyu@gmail.com>
2023-03-30 18:53:27 +0800

    Fix: compactLesson now will not function if the path is not index.html

--- src/plugins/CompactLesson.js
+++ src/plugins/CompactLesson.js
@@ -1,19 +1,21 @@
 export function compactLesson(isOpen, origin) {
-    if (isOpen) {
-        assignProperties("el-carousel__container", {
-            display: "flex",
-            flexDirection: "column",
-        });
-        assignProperties("el-carousel__item", {
-            display: "flex",
-            position: "relative",
-            overflow: "initial",
-            transform: "",
-        });
-        document.getElementsByClassName("header-control")[0].remove();
-    } else {
-        recoverElementByClassName("el-carousel__container", origin);
-        recoverElementByClassName("my-lesson-header", origin);
+    if (document.location.pathname === "/uclass/index.html") {
+        if (isOpen) {
+            assignProperties("el-carousel__container", {
+                display: "flex",
+                flexDirection: "column",
+            });
+            assignProperties("el-carousel__item", {
+                display: "flex",
+                position: "relative",
+                overflow: "initial",
+                transform: "",
+            });
+            document.getElementsByClassName("header-control")[0].remove();
+        } else {
+            recoverElementByClassName("el-carousel__container", origin);
+            recoverElementByClassName("my-lesson-header", origin);
+        }
     }
 }
 
